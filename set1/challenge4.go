a5481e5c47f5b1459661e476139081d728adafc9
Mariano Sorgente <mariano.sorgente@token.io>
2017-02-15 23:42:49 -0800

    Some work

--- set1/challenge4.go
+++ set1/challenge4.go
@@ -7,6 +7,21 @@ import (
   "os"
 )
 
+func BreakRKX(bytes []byte) {
+  key := make([]byte, len(bytes))
+
+  for char := 0; char < 256; char++ {
+    for i, _ := range key {
+      key[i] = byte(char)
+    }
+    res, _ := Xor(bytes, key)
+    calcd := Score(string(res))
+    if calcd >= 0.2 {
+      fmt.Println(calcd, string(res))
+    }
+  }
+}
+
 func challenge4() {
   dir, err := os.Getwd()
   if err != nil {
@@ -21,18 +36,7 @@ func challenge4() {
   scanner := bufio.NewScanner(file)
   for scanner.Scan() {
     bytes, _ := HexToBytes(scanner.Text())
-    key := make([]byte, len(bytes))
-
-    for char := 0; char < 256; char++ {
-      for i, _ := range key {
-        key[i] = byte(char)
-      }
-      res, _ := Xor(bytes, key)
-      calcd := Score(string(res))
-      if calcd >= 0.35 {
-        fmt.Println(calcd, string(res))
-      }
-    }
+    BreakRKX(bytes)
   }
   if err := scanner.Err(); err != nil {
     log.Fatal(err)
--- set1/challenge6.go
+++ set1/challenge6.go
@@ -1,8 +1,69 @@
-func CalculateHammingDistance(word1 string, word2 string) {
+package main
+
+import (
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "path/filepath"
+)
+
+func CalculateHammingDistance(word1 string, word2 string) (int, error) {
+  if len(word1) != len(word2) {
+    return 0, errors.New("Wrong length")
+  }
+  word1Bytes := []byte(word1)
+  word2Bytes := []byte(word2)
   substitutionsRequired := 0
-  for i, _ := range word1 {
-    if strings.CharAt(word1, i) != string.CharAt(word2, i) {
-      substitutionsRequired++
+  for i, _ := range word1Bytes {
+    if word1Bytes[i] != word2Bytes[i] {
+      diff := int(word1Bytes[i] ^ word2Bytes[i])
+      numDiff := 0
+      for diff != 0 {
+        numDiff += (diff & 0x1)
+        diff >>= 1
+      }
+      substitutionsRequired += numDiff
     }
   }
+  return substitutionsRequired, nil
+}
+
+func FindKeySize(dat []byte) int {
+  for KEYSIZE:=2; KEYSIZE<40; KEYSIZE++ {
+    total := 0.0
+    for i:=0; i<10; i++ {
+      for j:=i; j<10; j++ {
+        if i != j {
+          first := string(dat[i*KEYSIZE:(i+1)*KEYSIZE])
+          second := string(dat[j*KEYSIZE:(j+1)*KEYSIZE])
+          dist, _ := CalculateHammingDistance(first, second)
+          // fmt.Println(KEYSIZE, ": ", i, j, float64(dist)/float64(KEYSIZE))
+          total += float64(dist)/float64(KEYSIZE)
+        }
+      }        
+    }
+    fmt.Println(KEYSIZE, total)
+  }
+  return 6;
+}
+
+func main() {
+  absPath, _ := filepath.Abs("set1/ciphertext.txt")
+  dat, _ := ioutil.ReadFile(absPath)
+  KEYSIZE := FindKeySize(dat)
+  
+  var parsed [][]byte
+  numBlocks := len(dat)/KEYSIZE;
+  for index:=0; index<KEYSIZE; index++ {
+    indexSlice := []byte{}
+    for blockNum:=0; blockNum<numBlocks; blockNum++ {
+      indexSlice = append(indexSlice, dat[blockNum*KEYSIZE + index])
+    }
+    parsed = append(parsed, indexSlice)
+  }
+  for i, block := range parsed {
+    fmt.Println(i)
+    BreakRKX(block)
+  }
+  // fmt.Println(parsed)
 }
